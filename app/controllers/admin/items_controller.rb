6768571e000f5ded3200d7e1a1f71cbbe0c9cc5f
rikuyajima <psytmk2234@gmail.com>
2022-09-18 06:48:28 +0000

    [update]syouhinitirangamen

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,12 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @item = Item.find(params[:id])
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :net_price, :status)
   end
 end
