4ffc2c569edcff9859c6a12e1cf289a31c4b4f36
rikuyajima <psytmk2234@gmail.com>
2022-09-18 13:09:08 +0000

    [create]syouhinsinkigamen

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,20 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @genre = Genre.find(params[:genre_id])
+    @item = Item.new(item_params)
+    @item.genre_id = @genre.id
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      @itemss = Item.all
+      render 'index'
+    end
+  end
+
   def index
   end
 end
