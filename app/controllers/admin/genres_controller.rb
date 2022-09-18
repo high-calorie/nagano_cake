a9ff2481cbadae40a10513d59d1e9da4f8de0cad
EC2 Default User <ec2-user@ip-172-31-6-118.ap-northeast-1.compute.internal>
2022-09-18 15:47:08 +0000

    [fixed]remote name3

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,21 +1,15 @@
 class Admin::GenresController < ApplicationController
   def edit
-  @genre = Genre.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
 
   def update
-  @genre = Genre.find(params[:id])
-  if @genre.update(admin_params)
-  redirect_to request.referer
-  else
-  render :new
-  end
-  end
-
-  def destroy
-  @genre = Genre.find(params[:id])
-  @genre.destroy
-  redirect_to request.referer
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to request.referer
+    else
+      render :new
+    end
   end
 
 end
