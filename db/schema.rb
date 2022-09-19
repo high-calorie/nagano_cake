09f3002d24096255fdca3f662f54519dc8f27a1e
OONOSSHINPEI <110380501+OONOSSHINPEI@users.noreply.github.com>
2022-09-19 02:13:21 +0000

    [Finish]syouhinsyousai.11.1.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_18_061442) do
+ActiveRecord::Schema.define(version: 2022_09_16_115342) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -69,10 +69,9 @@ ActiveRecord::Schema.define(version: 2022_09_18_061442) do
     t.string "name"
     t.string "introduction"
     t.string "net_price"
+    t.boolean "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "genre_id"
-    t.boolean "status", default: true, null: false
   end
 
   create_table "order_details", force: :cascade do |t|
