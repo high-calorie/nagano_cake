18843ff7f258e2dcd49f7f461df413cfedc602b4
EC2 Default User <ec2-user@ip-172-31-6-118.ap-northeast-1.compute.internal>
2022-09-19 15:28:36 +0000

    [finish]janru-henshu

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,4 @@
 class Admin::GenresController < ApplicationController
-<<<<<<< HEAD
   def edit
     @genre = Genre.find(params[:id])
   end
@@ -7,20 +6,19 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
-      redirect_to request.referer
+      redirect_to admin_genres_path
     else
-      render :new
+      render :index
     end
   end
-=======
 
-    
+
     def index
         @genre = Genre.new
         @genres = Genre.all
     end
-        
-    
+
+
     def create
         @genre = Genre.new(genre_params)
         if @genre.save
@@ -28,21 +26,16 @@ class Admin::GenresController < ApplicationController
            redirect_to admin_genres_path
         else
           @genres = Genre.all
-          render :index 
+          render :index
         end
     end
-    
+
 private
-    
+
     def genre_params
     params.require(:genre).permit(:genre_name)
-  
+
     end
 
-
-
-
-    
->>>>>>> cd3a2554e8acd31ed6d334b189c2b30ad9ad9a8b
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-<<<<<<< HEAD
-    resources :genres, only: [:create, :new, :edit, :update, :destroy]
-
-    resources :items, only: [:index]
-=======
-    get 'genres/index'
-    get 'genres/edit'
-
-    resources :genres, only: [:create, :index]
+    resources :genres, only: [:index, :create, :new, :edit, :update, :destroy]
     resources :items, except: [:destroy]
->>>>>>> cd3a2554e8acd31ed6d334b189c2b30ad9ad9a8b
 
     get 'customers/show'
   end
@@ -26,7 +17,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
- 
+
 
 end
 
