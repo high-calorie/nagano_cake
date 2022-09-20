11d13045404fd2f85004e6bc999b13f3b249f051
KentoWakasa <103986115+KentoWakasa@users.noreply.github.com>
2022-09-20 15:31:51 +0000

    [create]public/homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   namespace :admin do
 
   resources :genres, only: [:index, :create, :new, :edit, :update, :destroy]
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
 
     get 'customers/show'
     get 'customers/edit'
-    patch 'customer/:id'
+    # patch 'customer/:id'
   end
 
 
