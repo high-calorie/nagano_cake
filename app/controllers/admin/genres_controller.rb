80c1a8b06e068158e6d4c636c4d6e9ad805aade1
KentoWakasa <103986115+KentoWakasa@users.noreply.github.com>
2022-09-20 01:43:31 +0000

    [Add]kaiin/sinkitourokugamen_p/r/new

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,38 +1,30 @@
 class Admin::GenresController < ApplicationController
 
-    
+
     def index
         @genre = Genre.new
         @genres = Genre.all
     end
-        
-    
+
+
     def create
-        @genre = Genre.new(genre_params)
-        if @genre.save
-           flash[:notice] = "ジャンルを追加しました"
-           redirect_to admin_genres_path
-        else
-          @genres = Genre.all
-          render :index 
-        end
-    end
-    
-    
-    def edit
-    
-  　end
-    
-    private
-    
-    def genre_params
-    params.require(:genre).permit(:name)
-  
+      @genre = Genre.new(genre_params)
+      if @genre.save
+         flash[:notice] = "ジャンルを追加しました"
+         redirect_to admin_genres_path
+      else
+        @genres = Genre.all
+        render :index
+     end
     end
 
-        
-    
-    
-    
+  #   def edit
+  # 　end
+
+    private
+
+    def genre_params
+      params.require(:genre).permit(:genre_name)
+    end
 
 end
