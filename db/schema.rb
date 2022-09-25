acf2bda31172d4bdf0a99b621c88abe994993d92
KentoWakasa <103986115+KentoWakasa@users.noreply.github.com>
2022-09-25 06:53:17 +0000

    [arrangement]route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_21_102133) do
 ActiveRecord::Schema.define(version: 2022_09_23_043906) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
