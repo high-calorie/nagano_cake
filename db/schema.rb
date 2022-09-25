32a3c16015111bd3125045f26c99b6c00f3b081e
rikuyajima <psytmk2234@gmail.com>
2022-09-25 08:44:17 +0000

    [fix]layoutsupdate

--- db/schema.rb
+++ db/schema.rb
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 2022_09_23_043906) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "introduction"
-    t.string "net_price"
+    t.integer "net_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "genre_id"
