50d3ef42bf5617ffecc7fe92b5d6365291876908
KentoWakasa <103986115+KentoWakasa@users.noreply.github.com>
2022-09-25 14:46:31 +0000

    [add]admin/homes

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
-    @orders = Order.all
-    @order_details = Oder_detail.find(params[:id])
+    @orders = Order.page(params[:page])
+    @order_details = OrderDetail.all
   end
 end
