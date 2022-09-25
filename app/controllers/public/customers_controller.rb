c2d6532aec8a540dc7ee97f3c6507f10d2757bc3
OONOSSHINPEI <110380501+OONOSSHINPEI@users.noreply.github.com>
2022-09-25 15:38:28 +0000

    [layout]mypage2

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,21 @@
 class Public::CustomersController < ApplicationController
     
-    def show
-        @customer = current_customer
-    end
+
+  def show
+      @customer = current_customer
+  end
     
-    
+  def unsubscribe
+    @customer = current_customer
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(withdrawal_flag: true)
+    reset_session
+    redirect_to root_path
+  end
+
  private
  
  def customer_params
