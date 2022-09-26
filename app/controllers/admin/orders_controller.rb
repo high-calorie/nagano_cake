354eeea4faac8e0ca7c57a9cbee44c2f125754ba
EC2 Default User <ec2-user@ip-172-31-6-118.ap-northeast-1.compute.internal>
2022-09-26 13:43:19 +0000

    for push

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,19 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
 		@order_details = @order.order_details
   end
+
+  def update
+		@order_detail = OrderDetail.find(params[:id])
+	  if @order_detail.update(order_detail_params)
+	    redirect_to admin_order_path(@order_detail.order)
+	  else
+	     render "show"
+	  end
+  end
+
+  private
+  def order_detail_params
+		  params.require(:order_detail).permit(:make_status)
+  end
+
 end
