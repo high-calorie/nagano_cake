22801ced65377b8f00d00c866291b952691e206c
EC2 Default User <ec2-user@ip-172-31-6-118.ap-northeast-1.compute.internal>
2022-09-26 19:20:19 +0000

    tsuika

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+   devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
   namespace :admin do
     get 'orders/show'
   end
@@ -49,14 +58,6 @@ Rails.application.routes.draw do
 
   end
 
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
 
 end
 
